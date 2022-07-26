ff63e5ad5a2d272657bf1273754a6c5d9ee826e6
Kabir Chhabra <54521120+kabirchhabra@users.noreply.github.com>
2022-07-26 09:16:19 +0530

    Time: 4 ms (89.81%), Space: 13.7 MB (77.69%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -38,19 +38,36 @@
 //     }
 // };
 
-//using tabulation
+// //using tabulation
+// class Solution{
+// public:
+//     int minCostClimbingStairs(vector<int> &cost){
+//         int n = cost.size();
+//         vector<int> dp(n+1, -1);
+        
+//         dp[0] = cost[0];
+//         dp[1] = cost[1];
+//         for(int i=2;i<n;i++){
+//             dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+//         }
+        
+//         return min(dp[n-1], dp[n-2]);
+//     }
+// };
+
+
+// Space Optimization in tabulation
 class Solution{
 public:
     int minCostClimbingStairs(vector<int> &cost){
-        int n = cost.size();
-        vector<int> dp(n+1, -1);
-        
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i=2;i<n;i++){
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+        for(int i=2;i<cost.size();i++){
+            int curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
         }
         
-        return min(dp[n-1], dp[n-2]);
+        return min(prev1, prev2);
     }
 };
