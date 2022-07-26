0a3935a460aa7a8eb31fc47e5324d5f841b83a61
Kabir Chhabra <54521120+kabirchhabra@users.noreply.github.com>
2022-07-26 09:11:32 +0530

    Time: 7 ms (81.79%), Space: 13.8 MB (48.06%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -17,23 +17,40 @@
 //     }
 // };
 
-// Using DP
-class Solution{
-private:
-    int solve(vector<int> &cost, vector<int> &dp, int n){
-        if(n==0 || n==1) return dp[n]=cost[n];
-        if(dp[n]!=-1) return dp[n];
+// // Using memoization
+// class Solution{
+// private:
+//     int solve(vector<int> &cost, vector<int> &dp, int n){
+//         if(n==0 || n==1) return dp[n]=cost[n];
+//         if(dp[n]!=-1) return dp[n];
         
-        int ans = cost[n] + min(solve(cost, dp, n-1), solve(cost, dp, n-2));
-        dp[n] = ans;
-        return dp[n];
-    }
+//         int ans = cost[n] + min(solve(cost, dp, n-1), solve(cost, dp, n-2));
+//         dp[n] = ans;
+//         return dp[n];
+//     }
+// public:
+//     int minCostClimbingStairs(vector<int> &cost){
+//         int n = cost.size();
+//         vector<int> dp(cost.size(), -1);
+//         int ans = min(solve(cost, dp, n-1), solve(cost, dp, n-2));
+        
+//         return ans;
+//     }
+// };
+
+//using tabulation
+class Solution{
 public:
     int minCostClimbingStairs(vector<int> &cost){
         int n = cost.size();
-        vector<int> dp(cost.size(), -1);
-        int ans = min(solve(cost, dp, n-1), solve(cost, dp, n-2));
+        vector<int> dp(n+1, -1);
         
-        return ans;
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+        
+        return min(dp[n-1], dp[n-2]);
     }
 };
